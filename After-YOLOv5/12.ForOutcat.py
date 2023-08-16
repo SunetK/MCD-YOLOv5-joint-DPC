a64188690397f03348f4c3737bc33f3a8837dab7
SunsetK <95158853+SunetK@users.noreply.github.com>
2023-08-16 15:45:17 +0800

    Update 12.ForOutcat.py

--- After-YOLOv5/12.ForOutcat.py
+++ After-YOLOv5/12.ForOutcat.py
@@ -3,7 +3,6 @@ import pandas as pd
 path = 'table/new/synthetic_outcat_0099.csv'
 data = pd.read_csv(path, sep='\t')
 
-
 with open('table/new/0099_realcen.txt', 'a+') as f:
     for line in data.values:
         f.write((str(line[4])+'\t'+str(line[5])+'\t'+(str(line[6])+'\n')))
