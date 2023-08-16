3d59b4e1930928fd09a20ec74f66fd957837e84e
SunsetK <95158853+SunetK@users.noreply.github.com>
2023-08-16 15:51:30 +0800

    Update mainDensityClust.m

--- DPC/mainDensityClust.m
+++ DPC/mainDensityClust.m
@@ -2,14 +2,10 @@ clear
 close all;
 clc;
 
-aa = load('3D_data\G100\G100_3D_0031_CenStrong.txt');
+aa = load('3D_data\G100\G100_3D_0099_CenStrong.txt');
 xy = [aa(:,1) , aa(:,2) , aa(:,3) ];
 X = xy;
 xy = floor(xy);
-% A1= zeros(size(data));
-% for ii = 1:size(xy,1)
-    % A1(xy(ii,2)+1,xy(ii,1)+1,xy(ii,3)+1) = data(xy(ii,2)+1,xy(ii,1)+1,xy(ii,3)+1);
-% end
 
 figure(1);
 plot3(X(:, 1), X(:, 2), X(:, 3), 'b.');
@@ -18,22 +14,8 @@ ylabel('Dim 2');
 zlabel('Dim 3');
 title('Original Data Set');
 
-%% Settings of System Parameters for DensityClust
-dist = pdist2(X, X); % [NE, NE] matrix (this case may be not suitable for large-scale data sets)
-% average percentage of neighbours, ranging from [0, 1]
-% as a rule of thumb, set to around 1%-2% of NE (see the corresponding *Science* paper for more details)
-percNeigh = 0.02;
-% 'Gauss' denotes the use of Gauss Kernel to compute rho, and
-% 'Cut-off' denotes the use of Cut-off Kernel.
-% For large-scale data sets, 'Cut-off' is preferable owing to computational efficiency,
-% otherwise, 'Gauss' is preferable in the case of small samples (especially with noises).
-%kernel = 'Cut-off';
-% set critical system parameters for DensityClust
-
 dc = 3;
 rho = [aa(:,4)];
-
-%[dc, rho] = paraSet(dist, percNeigh, kernel, dc);     %rho的读取
 
 figure(2);
 plot(rho, 'b*');
@@ -41,12 +23,11 @@ xlabel('ALL Data Points');
 ylabel('\rho');
 title('Distribution Plot of \rho');
 
-%% Density Clustering
 isHalo = 1; 
 [numClust, clustInd, centInd, haloInd] = densityClust(dist, dc, rho, isHalo);
 save('densityClust.mat', 'numClust', 'clustInd', 'centInd', 'haloInd');
 figure(3);
-bb = load('data_f1\ppt\Real_data\new\0031_realcen.txt');
+bb = load('Real_data\G100\0099_realcen.txt');
 cen = [bb(:,1) , bb(:,2) , bb(:,3)];
 CEN = cen;
 cmap = colormap;
